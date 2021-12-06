41f27d2a61b5834b845e604deb37f0bf00a1e566
Matthew <mattj20@gmail.com>
2021-12-05 20:08:12 -0800

    Created and added helpers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,11 +4,20 @@ class ApplicationController < Sinatra::Base
     configure do
         set :views, 'app/views'
         enable :sessions
-        set :session_secret, SecureRandom.hex(64)
+        set :session_secret, 'password_security'
     end
 
     get '/' do
         erb :index
     end
 
+    helpers do
+		def logged_in?
+			!!current_user
+		end
+
+		def current_user
+			@current_user ||= User.find_by(:id => session[:user_id]) if session[:user_id]
+		end
+	end
 end
