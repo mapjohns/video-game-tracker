d7499116bf4d6462a952dae6b96ed8bd6a105f5c
Matthew <mattj20@gmail.com>
2021-12-07 20:04:54 -0800

    Added pages for creation of games

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,2 +1,23 @@
 class GamesController < ApplicationController
+    get '/games' do
+        binding.pry
+        @user = current_user
+        erb :'games/index'
+    end
+
+    get '/games/new' do
+        @user = current_user
+        erb :'games/new'
+    end
+
+    post '/games' do
+        @user = current_user.games.create(params)
+        redirect "/games"
+    end
 end
+
+# t.string :title
+# t.integer :rating
+# t.string :review
+# t.string :status
+# t.integer :user_id
