c86ff9984914eb07d5cbe0e611562dce84deb13d
Matthew <mattj20@gmail.com>
2021-12-08 19:53:47 -0800

    Updated values

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,6 +1,5 @@
 class GamesController < ApplicationController
     get '/games' do
-        binding.pry
         @user = current_user
         erb :'games/index'
     end
@@ -14,6 +13,22 @@ class GamesController < ApplicationController
         @user = current_user.games.create(params)
         redirect "/games"
     end
+
+    get '/games/:id' do
+        @game = current_user.games.find_by(:id=>params[:id])
+        erb :'games/show'
+    end
+
+    get '/games/:id/edit' do
+        @game = current_user.games.find_by(:id=>params[:id])
+        erb :'games/edit'
+    end
+
+    patch '/games/:id' do
+        @game = current_user.games.find_by(:id=>params[:id])
+        @game.update(title: params[:title], rating: params[:rating], review: params[:review], status: params[:status])
+        redirect "/games/#{@game.id}"
+    end
 end
 
 # t.string :title
@@ -21,3 +36,4 @@ end
 # t.string :review
 # t.string :status
 # t.integer :user_id
+##Add Date Started and Date Completed
